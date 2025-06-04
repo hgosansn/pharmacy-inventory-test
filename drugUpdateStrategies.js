10c7f0822dc4cf8f9189fafd6f99be64ff7d85c8
Hgosansn <hugo.sanson@gmail.com>
2025-06-04 09:15:51 +0200

    chore: factor strategy code and improve readability

--- drugUpdateStrategies.js
+++ drugUpdateStrategies.js
@@ -1,3 +1,7 @@
+const MAX_BENEFIT = 50;
+const limitBenefit = (benefit) => Math.max(0, Math.min(MAX_BENEFIT, benefit));
+const decrementExpiresIn = (expiresIn) => expiresIn - 1;
+
 export class DrugUpdateStrategy {
   update(drug) {
     throw new Error(`${drug.name} update method must be implemented!`);
@@ -6,68 +10,64 @@ export class DrugUpdateStrategy {
 
 export class DefaultDrugStrategy extends DrugUpdateStrategy {
   update(drug) {
-    if (drug.benefit > 0) {
-      drug.benefit = drug.benefit - 1;
+    const expiresIn = decrementExpiresIn(drug.expiresIn);
+    let benefit = drug.benefit;
+    if (benefit > 0) {
+      benefit -= 1;
     }
-    drug.expiresIn = drug.expiresIn - 1;
-    if (drug.expiresIn < 0 && drug.benefit > 0) {
-      drug.benefit = drug.benefit - 1;
+    if (expiresIn < 0) {
+      benefit -= 1;
     }
-    return drug;
+    return { ...drug, expiresIn, benefit: limitBenefit(benefit) };
   }
 }
 
 export class HerbalTeaStrategy extends DrugUpdateStrategy {
   update(drug) {
-    if (drug.benefit < 50) {
-      drug.benefit = drug.benefit + 1;
+    const expiresIn = decrementExpiresIn(drug.expiresIn);
+    let benefit = drug.benefit + 1;
+    if (expiresIn < 0) {
+      benefit += 1;
     }
-    drug.expiresIn = drug.expiresIn - 1;
-    if (drug.expiresIn < 0 && drug.benefit < 50) {
-      drug.benefit = drug.benefit + 1;
-    }
-    return drug;
+    return { ...drug, expiresIn, benefit: limitBenefit(benefit) };
   }
 }
 
 export class FervexStrategy extends DrugUpdateStrategy {
   update(drug) {
-    if (drug.benefit < 50) {
-      drug.benefit = drug.benefit + 1;
-      if (drug.expiresIn < 11 && drug.benefit < 50) {
-        drug.benefit = drug.benefit + 1;
-      }
-      if (drug.expiresIn < 6 && drug.benefit < 50) {
-        drug.benefit = drug.benefit + 1;
-      }
+    const expiresIn = decrementExpiresIn(drug.expiresIn);
+    let benefit = drug.benefit + 1;
+    if (drug.expiresIn < 11) {
+      benefit += 1;
     }
-    drug.expiresIn = drug.expiresIn - 1;
-    if (drug.expiresIn < 0) {
-      drug.benefit = 0;
+    if (drug.expiresIn < 6) {
+      benefit += 1;
     }
-    return drug;
+    if (expiresIn < 0) {
+      benefit = 0;
+    }
+    return { ...drug, expiresIn, benefit: limitBenefit(benefit) };
   }
 }
 
 export class MagicPillStrategy extends DrugUpdateStrategy {
   update(drug) {
     // Magic Pill does not change.
-    return drug;
+    return { ...drug };
   }
 }
 
 export class DafalganStrategy extends DrugUpdateStrategy {
   update(drug) {
-    if (drug.benefit > 0) {
-      drug.benefit = drug.benefit - 2;
-      if (drug.benefit < 0) drug.benefit = 0;
+    const expiresIn = decrementExpiresIn(drug.expiresIn);
+    let benefit = drug.benefit;
+    if (benefit > 0) {
+      benefit -= 2;
     }
-    drug.expiresIn = drug.expiresIn - 1;
-    if (drug.expiresIn < 0 && drug.benefit > 0) {
-      drug.benefit = drug.benefit - 2;
-      if (drug.benefit < 0) drug.benefit = 0;
+    if (expiresIn < 0) {
+      benefit -= 2;
     }
-    return drug;
+    return { ...drug, expiresIn, benefit: limitBenefit(benefit) };
   }
 }
 
